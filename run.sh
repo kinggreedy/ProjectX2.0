6c982071b364fa11882e18fdfc7405d906181232
Chau Nguyen <6055140+kinggreedy@users.noreply.github.com>
2024-09-21 12:37:24 +0000

    Removed debugging information from run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 source name.sh
-
-echo "Running Docker container with image: ${IMAGE}"
-
-docker images  # List available Docker images to check if ${IMAGE} was built
-
-docker run --rm ${IMAGE} || { echo "Docker run failed"; exit 1; }
+docker run --rm ${IMAGE}
